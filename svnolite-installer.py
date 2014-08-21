58fddfc4ef2415b1ec71339ed1645b3a7408e3d3
insomia <tanya.kravchenko.v@gmail.comu>
2014-08-21 17:05:10 +0300

    add modified_conf_svnserve, post_commit functions

--- svnolite-installer.py
+++ svnolite-installer.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
-import os, errno
+import os, errno, re
 import subprocess
 import shutil
 import pysvn
-####ToDO: check if pysvn is installed
+import ConfigParser
+import datetime
+####TODO: check if pysvn is installed
 
 home_svn_dir="/home/svn"
 home_svnolite_dir="/home/svn/svnolite"
@@ -14,9 +16,6 @@ repo_home="/home/svn/repositories"
 client = pysvn.Client()
 client.commit_info_style = 1
 
-def dir_if_not_exist_create(directory):
-	if not os.path.exists(directory):
-               	os.makedirs(directory)
 
 def custimize_svn_admin():
 	url = "file://"+repo_home+"/svn-admin"
@@ -36,11 +35,19 @@ def create_svn_admin():
         (output, err) = p.communicate()
 	custimize_svn_admin()
 	copy_configs(home_svnolite_dir+"/template_conf/", home_svn_dir+"/")
+	print current_time()+" svn-admin repository created and customized"
+	if not os.path.exists(repo_home+'/svn-admin/hooks/post-commit'):
+		post_commit()
 	#print "Revision is", output
 
-def copy_configs(src, dst):
-	for i in [f for f in os.listdir(src)]:
-		shutil.copy(src+i, dst)	
+def post_commit():
+	with open(repo_home+'/svn-admin/hooks/post-commit','w+') as f:
+		f.write('#!/bin/bash \n')
+		f.write('python /home/svn/svnolite/svnolite-installer.py >> /home/svn/svnolite/svnolite.log')
+	os.chmod(repo_home+'/svn-admin/hooks/post-commit', 0755)
+
+def current_time():
+	return str(datetime.datetime.now())
 
 def svn_admin_update():
 	os.chdir(work_dir)
@@ -53,15 +60,21 @@ def svn_admin_update():
 	if current_rev_num_WC < current_rev_num:
 		print "Now will be executed 'svn update'"
 		client.update(work_dir, recurse=True)
+		print current_time()+" svn-admin repository was updated"
 		if check_if_modified(current_rev_num):
 			for i in check_if_modified(current_rev_num):
 				if i.split('/')[1] == 'keys':
 					authorize_key_gen()
-				elif i.split('/')[1] == '/conf/svnserve.conf':
-					
-					##TODO
+				elif i.split('/')[2] == 'authz':
+					modified_authz()	
+					print "Authz was modified"
+				elif i.split('/')[2] == 'svnserve.conf':
 					print "'Conf' was changed"
-				
+					modified_conf_svnserve()
+				else:
+					print "Error: strange modification"
+		else:
+			print "No changes there."				
 	
 def check_if_modified(revision_number):	
 	revision = pysvn.Revision(pysvn.opt_revision_kind.number, revision_number)
@@ -74,7 +87,7 @@ def check_if_modified(revision_number):
     		for j in paths:
       			if j['action'] in ['A', 'M', 'U', 'D']:
         			diff_paths.append(str(unicode(j['path'])))
-        print  diff_paths
+        #print  diff_paths
 	if diff_paths == []:
 		print "There is no changes"
 		return False
@@ -82,58 +95,61 @@ def check_if_modified(revision_number):
 		return diff_paths
 				
 	
-# This function check if exist modification on local work copy before commit
-#def check_if_modified(filename):
-#	dict_status = {}
-#	for i in [f for f in os.listdir(filename) if not f.startswith('.')]:
-#		#status = str( client.status(filename+i)[0].text_status)
-#		status = str( client.status(filename+i, recurse=True, update=False)[0].text_status)
-#		if status != 'normal':
-#			dict_status[filename+i] = status
-#	if dict_status:
-#		print dict_status
-#		return True   #is empty
-#	else:	
-#		print dict_status
-#		return False   #is not empty
-
 def modified_conf_svnserve():
-	svnserve_config = open(work_dir+"/conf/svnserve.con", 'r')
-	###TODO
+	os.chdir(work_dir+'/conf')
+	valid_value = {'general': {'anon-access':'write|read|none',
+                                'auth-access':'write|read|none',
+                                'authz-db': 'authz|/.+/authz'}}
+	try:   
+        	parser = ConfigParser.SafeConfigParser()
+        	parser.read(work_dir+"/conf/svnserve.conf")
+	except ConfigParser.ParsingError, err:
+                print 'Could not parse:', err
+
+	if parser.sections() != valid_value.keys():
+        	print "Name of section: [general] was changed or section was deleted"
+	else:  
+        	if parser.options('general') != valid_value['general'].keys():
+                	print "You add/delete or modified standard options! Must be: 'anon-access', 'auth-access'and 'authz-db'"
+        	else:  
+                	for i in parser.options('general'):
+                        	if not re.match(valid_value['general'][i],parser.get('general',i)):
+                                	print "Error: Bad value in {0}. Accept values: 'anon-access':'write|read|none', 'auth-access':'write|read|none', 'authz-db': filename".format(i.upper())
+                	if not os.path.exists(parser.get('general','authz-db')):
+                        	print "File {0} dont exist!".format(parser.get('general','authz-db'))
+	shutil.copy(work_dir+"/conf/svnserve.conf", home_svn_dir)
  
-def modefied_auth():
+def modified_authz():
 	order_list = []
-	for i in [re.findall('^[^#].+:.*$',line) for line in open(work_dir+"conf/auth")]:
+	for i in [re.findall('^[^#].+:.*$',line) for line in open(work_dir+"/conf/authz")]:
 		if i != []:
 			order_list.append(i[0])
 	print order_list
 	for i in range(0, len(order_list)):
 		order_list[i] = re.sub('["["]','', order_list[i]).split(":")[0] 
 				
-	order_list = lisr(set(order_list))
+	order_list = list(set(order_list))
         for i in order_list:
 		if testURL("file://"+repo_home+"/"+i):
-			print "Repo " + i + "exist."
+			print "Repo " + i + " exist."
 		else:
 			print "Repository {0} don`t exist. {0} will be created".format(i)
 			os.chdir(repo_home)
 			p = subprocess.Popen("svnadmin create "+i, stdout=subprocess.PIPE, shell=True)
         		(output, err) = p.communicate() 
 		
+	shutil.copy(work_dir+"/conf/authz", home_svn_dir)	
 
 def testURL(url):
 	try:
         	client.info2(url)
         	return True
 	except pysvn.ClientError, ce:
-        	if ('non-existant' in ce.args[0]) or ('Host not found' in ce.args[0]):
+        	if ('Unable to open repository' in ce.args[0]) or ('Host not found' in ce.args[0]):
 			print "Repo dont exist"
             		return False
         	else:
             		raise ce
-###TODO: update, if exist changes: 1) in conf:
-		## a) svnserve.conf: restart svnserve?
-		## b) auth: 
 	
 ##TODO: arg to input: in if i.split('/')[2].split('.')[0]
 def authorize_key_gen():
@@ -154,6 +170,31 @@ def authorize_key_gen():
 
         authozile_file.close()
 
+def dir_if_not_exist_create(directory):
+	if not os.path.exists(directory):
+               	os.makedirs(directory)
+
+def copy_configs(src, dst):
+	for i in [f for f in os.listdir(src)]:
+		shutil.copy(src+i, dst)	
+
+# This function check if exist modification on local work copy before commit
+#def check_if_modified(filename):
+#	dict_status = {}
+#	for i in [f for f in os.listdir(filename) if not f.startswith('.')]:
+#		#status = str( client.status(filename+i)[0].text_status)
+#		status = str( client.status(filename+i, recurse=True, update=False)[0].text_status)
+#		if status != 'normal':
+#			dict_status[filename+i] = status
+#	if dict_status:
+#		print dict_status
+#		return True   #is empty
+#	else:	
+#		print dict_status
+#		return False   #is not empty
+#
+#Bag 1: When commit, and in svnserve.conf is bad value!
+#Bag 2: recreate log file! 
 if not os.path.exists(repo_home+"/svn-admin"):
 	create_svn_admin()
 else:
