d8455113f0f913e4251861929fc8496c4dadcc7e
ins0mia <tanya.kravchenko.v@gmail.com>
2014-09-21 20:56:04 +0300

    add arguments, change authorize_key_gen function

--- svnolite.py
+++ svnolite.py
@@ -12,17 +12,20 @@ import sys, getopt
 ####TODO: set correct umask(750)		check
 ####TODO: post-commit - in svnolite.log		check
 ####TODO: change name to svnolite.py		+
-###TODO: del path "home/svn" in 2 place		+
+###TODO: del path "home/svn" in 2 place		
 ###TODO: create dir .ssh if not exist		+
 ###TODO: 3 mode: install + admin key + in authz give permisions; post-commit; reset admin key
 ###TODO: help (modes + link to github)
 ###TODO: fix adding keys after commit
 
 home_svn_dir=str(os.path.split(os.getcwd())[0])
+#home_svn_dir=home_svn_dir1
 home_svnolite_dir=home_svn_dir+"/svnolite-dir"
 work_dir=home_svn_dir+"/bin"
 home_dir_ssh=home_svn_dir+"/.ssh"
 repo_home=home_svn_dir+"/repositories"
+
+print home_svn_dir
 
 # filesize in MB
 log_file_rotate_size=10
@@ -65,10 +68,15 @@ def create_svn_admin():
 	#print "Revision is", output
 
 def post_commit():
+	#os.chdir(home_svnolite_dir)
+	current_path = os.path.realpath(__file__)	
 	with open(repo_home+'/svn-admin/hooks/post-commit','w+') as f:
 		f.write('#!/bin/bash \n')
-		f.write('logger -t [svnolite: ] -f ' + home_svnolite_dir+'/svnolite.log ' + '`python ' + os.path.realpath(__file__))
+		f.write('logger -t [svnolite: ] -f ' + home_svnolite_dir+'/svnolite.log ' + '`python ' + home_svnolite_dir + '/svnolite.py -u `')
+		#f.write('python ' +home_svnolite_dir+ '/svnolite.py -u')
+		#f.write('`python ' + os.path.realpath(__file__)+' -u ` >> '+home_svnolite_dir+'/svnolite.log ')
 	os.chmod(repo_home+'/svn-admin/hooks/post-commit', 0755)
+	logger.info('File post-commit was created!')
 
 def current_time():
 	return str(datetime.datetime.now())
@@ -88,7 +96,7 @@ def svn_admin_update():
 		if check_if_modified(current_rev_num):
 			for i in check_if_modified(current_rev_num):
 				if i.split('/')[1] == 'keys':
-					authorize_key_gen()
+					authorize_key_gen(work_dir+"/keys/"+i.split('/')[2])
 				elif i.split('/')[2] == 'authz':
 					modified_authz()	
 					logger.info("'Authz' file was modified")
@@ -111,7 +119,7 @@ def check_if_modified(revision_number):
     		for j in paths:
       			if j['action'] in ['A', 'M', 'U', 'D']:
         			diff_paths.append(str(unicode(j['path'])))
-        #print  diff_paths
+        logger.info("Modification:"+ str(diff_paths))
 	if diff_paths == []:
 		logger.info("There is no changes")
 		return False
@@ -176,7 +184,7 @@ def testURL(url):
             		raise ce
 	
 ##TODO: arg to input: in if i.split('/')[2].split('.')[0]
-def authorize_key_gen():
+def authorize_key_gen(path_to_file):
 	dir_if_not_exist_create(home_svn_dir+"/.ssh")
 	os.chdir(work_dir)
         if os.path.exists(home_dir_ssh+"/authorized_keys"):
@@ -185,13 +193,15 @@ def authorize_key_gen():
                 logger.info("Sorry, but file authorized_keys is not found. This file will be created")
                 open(home_dir_ssh+"/authorized_keys", 'a').close()
 
-        keys_list = [f for f in os.listdir(work_dir+"/keys") if not f.startswith('.')]
+        #keys_list = [f for f in os.listdir(work_dir+"/keys") if not f.startswith('.')]
         authozile_file = open (home_dir_ssh+"/authorized_keys", 'w+')
 
-        for i in keys_list:
-                keys = open(work_dir+"/keys/"+i, 'r')
-                authozile_file.write('command="svnserve -t -r {0} --config-file=/home/svn/svnserve.conf --tunnel-user={1}",no-port-forwarding,no-agent-forwarding,no-X11-forwarding,no-pty '.format(repo_home, i.split(".")[0]) + keys.read())
-                keys.close()    
+        #for i in keys_list:
+        #	keys = open(work_dir+"/keys/"+i, 'r')
+        #	authozile_file.write('command="svnserve -t -r {0} --config-file=/home/svn/svnserve.conf --tunnel-user={1}",no-port-forwarding,no-agent-forwarding,no-X11-forwarding,no-pty '.format(repo_home, i.split(".")[0]) + keys.read())
+        keys = open(path_to_file, 'r')
+        authozile_file.write('command="svnserve -t -r {0} --config-file={1} --tunnel-user={2}",no-port-forwarding,no-agent-forwarding,no-X11-forwarding,no-pty '.format(repo_home, home_svn_dir+'/svnserve.conf', os.path.basename(path_to_file).split(".")[0]) + keys.read())
+        keys.close()    
 
         authozile_file.close()
 
@@ -219,8 +229,45 @@ def copy_configs(src, dst):
 #		return False   #is not empty
 #
 #Bag 1: When commit, and in svnserve.conf is bad value!
-#Bag 2: recreate log file! 
-if not os.path.exists(repo_home+"/svn-admin"):
-	create_svn_admin()
-else:
-	f_not_exist_createsvn_admin_update()
+#Bag 2: recreate log file!
+ 
+def main(argv):
+	try:
+		opts, args = getopt.getopt(argv,"hiuk:",["install=","update=","help","key="])
+	except getopt.GetoptError:
+		print 'svnolite.py [-i] [-u] [-k </path/to/admin/key>] [-h]'
+		sys.exit(2)
+	for opt, arg in opts:
+		#print opt
+		if opt == '-h':
+			print '''Using of svnolite.py: \n -i  -  install \n -u  -  update (mean post-commit): \n -k /FULL/path/to/admin/key \n -h  -  help'''
+			sys.exit()
+		elif opt in ("-i", "--install"):
+			#print "installation . . ."
+			home_svn_dir1=str(os.path.split(os.getcwd())[0])
+			#global home_svn_dir
+			home_svn_dir=home_svn_dir1
+			create_svn_admin()	
+		elif opt in ("-u", "--update"):
+			#print "updating"
+			svn_admin_update()
+		elif opt in ("-k", "--key"):
+			authorize_key_gen(arg)
+			f = open(home_svn_dir+'/authz', 'r')
+			liles=f.readlines()
+			lines[lines.index('[svn-admin:/]\n')+1] = os.path.basename(path).split(".")[0]+ ' = rw' + '\n'
+			f = open(home_svn_dir+'/authz', 'w')
+			f.writelines(lines)
+			f.close()
+			
+			#with open (home_svn_dir+'/authz','w+') as f:
+			#	f.write('')
+			#os.path.basename(arg).split(".")[0]			
+
+if not len(sys.argv) > 1:
+	print "Enter some arguments! Usage: svnolite.py [-i] [-u] [-k </path/to/admin/key>] [-h]"
+	sys.exit()
+else:	
+	main(sys.argv[1:])
+#if __name__ == "__main__":
+#   main(sys.argv[1:])
